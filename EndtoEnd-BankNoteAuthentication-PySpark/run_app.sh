0b9197cb58a1151b51fcb14a67736092ed82afea
Ashwin K Raghu <65014691+holdmygithub@users.noreply.github.com>
2021-06-03 01:51:41 +0530

    Delete EndtoEnd-BankNoteAuthentication-PySpark directory

--- EndtoEnd-BankNoteAuthentication-PySpark/run_app.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-( sudo docker start --attach banknotecont ) || (( sudo docker run -p 8501:8501 --name banknotecont banknote ) || ((( echo "Building docker image" ) && ( sudo docker build -t banknote . )) && ( sudo docker run -p 8501:8501 --name banknotecont banknote )))
